2ec470e4d4f861c9173cea68511b5d366a9fe430
Andrew-Roide <andrew.roide@hotmail.com>
2024-07-23 01:18:23 +0000

    added google fonts

--- my-app/src/components/Home.tsx
+++ my-app/src/components/Home.tsx
@@ -2,21 +2,25 @@
 export default function Home() {
   return (
     <>
-    <section id="home">
-      <div className="flex flex-col h-screen">
-        <main className="flex flex-grow items-center justify-center text-center p-4">
-          <div className="italic">
-            <h1 className="text-customGrey text-4xl sm:text-7xl font-bold mb-4">
-              Andrew John Roide
-            </h1>
-            <p className="text-customGrey text-lg sm:text-xl font-medium max-w-3xl indent-40">
-              - A Lifelong Enthusiast Turning Curiosity into Craftsmanship as a
-              <span className="text-customBlue text-2xl"> Full Stack Web Developer</span>
-            </p>
-          </div>
-        </main>
-      </div>
-    </section>
+      <section id="home">
+        <div className="flex flex-col h-screen">
+          <main className="flex flex-grow items-center justify-center text-center p-4">
+            <div className="text-customGrey">
+              <h1 className="text-4xl sm:text-8xl font-bold mb-4 font-bilbo">
+                Andrew John Roide
+              </h1>
+              <p className="text-lg sm:text-xl font-medium max-w-3xl indent-40 font-montserrat italic">
+                - A Lifelong Enthusiast Turning Curiosity into Craftsmanship as
+                a
+                <span className="text-customBlue text-2xl">
+                  {" "}
+                  Full Stack Web Developer
+                </span>
+              </p>
+            </div>
+          </main>
+        </div>
+      </section>
     </>
   );
 }
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -9,6 +9,10 @@ export default {
       colors: {
         customBlue: "#4FC3F7",
         customGrey: "#E0E0E0"
+      },
+      fontFamily: {
+        bilbo: ['Bilbo', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif']
       }
     },
   },
