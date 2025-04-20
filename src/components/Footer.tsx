573b46d857f963ace98f57a2b09b63dd74dcda34
Andrew-Roide <andrew.roide@hotmail.com>
2025-04-20 01:02:06 +0000

    updated copyright year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="text-customBlue flex justify-center m-10">
       <div>
-        <p>&copy; 2024 Andrew Roide. All rights reserved.</p>
+        <p>&copy; 2025 Andrew Roide. All rights reserved.</p>
       </div>
     </footer>
   );
